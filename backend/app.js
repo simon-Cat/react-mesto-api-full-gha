944d275c7b1d8af692d347b80941cc8578ebf057
GregoMur <GregoMur@yandex.ru>
2023-07-09 20:58:27 +0300

    feat: add handle method OPTIONS

--- backend/app.js
+++ backend/app.js
@@ -30,12 +30,25 @@ const allowedCors = [
   'http://murtazaev-mesto.nomoredomains.monster',
   'localhost:3000',
 ];
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 app.use((req, res, next) => {
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const { method } = req;
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+  }
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы с этими заголовками
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    // завершаем обработку запроса и возвращаем результат клиенту
+    return res.end();
+}
 
   next();
 });
